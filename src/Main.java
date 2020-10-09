0c74a826cdd59ee3394d3f2e9c56eca969223bcd
Bartlomiej Lorenowicz <barteklorenowicz258@gmail.com>
2020-10-09 22:10:59 +0200

    Zadanie uzupełnienie

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    /*
+Klasa to … szablon lub instrukcja obsługi dla danego obiektu.
+Obiekt to … jest instancją klasy, zawiera dane metody służące do wykonywania określonych zadań.
+Zmienna to … symbol, ktróy może przyjmować różne wartości.
+Każda zmienna musi mieć określony …, TYP np. int, albo String.
+Jeżeli deklarację zmiennej poprzedzimy słowem final, to … będzie ona stałą, nie będzie można jej zmienić.
+W klasie mogą się znajdować METODY, OBIEKTY i KONSTRUKTORY
+… są wywoływane przez operator …NEW i służą do tworzenia nowych obiektów.
+Jeśli w klasie zdefiniowano chociaż jeden … Konstruktor to kompilator nie wygeneruje dla nas … domyślnego.
+Zakładając, że istnieje klasa Car, to przy zapisie:
+Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);
+Car car2 = car1;​
+car1 i car2 to dwie referencje… wskazujące na ten sam …obiekt
+     */
+}
